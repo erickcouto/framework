c65654d5e2ee143bf54542637ab594ef60a3c72d
Erick Alves <erick@erickalves.com.br>
2024-05-22 12:35:10 -0300

    teste de proptype

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { TouchableOpacity, Text } from "react-native";
+import PropTypes from "prop-types";
 
 const Button = ({ title, onPress }) => {
   return (
@@ -8,5 +9,8 @@ const Button = ({ title, onPress }) => {
     </TouchableOpacity>
   );
 };
-
+Button.propTypes = {
+  title: PropTypes.string.isRequired,
+  onPress: PropTypes.func.isRequired,
+};
 export default Button;
